1944e1bc5e6fbe8c6f9eee0ba84739a795ced751
dhilipsiva <dhilipsiva@gmail.com>
2015-05-10 12:52:57 +0530

    Update Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,8 @@
 /* global require, module */
 
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
+  assetRev = require('broccoli-asset-rev'),
+  htmlmin = require('broccoli-htmlmin');
 
 var app = new EmberApp({
  storeConfigInMeta: false
@@ -10,4 +12,21 @@ var app = new EmberApp({
 app.import('bower_components/socket.io-client/socket.io.js');
 app.import('bower_components/ember-sockets/dist/ember-sockets.js');
 
-module.exports = app.toTree();
+var tree = app.toTree();
+
+if (app.env == 'production') {
+  tree = assetRev(tree, {
+    enabled: true,
+    extensions: ['js', 'css', 'png', 'jpg', 'gif'],
+    replaceExtensions: ['html', 'js', 'css'],
+    prepend: '//d2n6qxtmzlogze.cloudfront.net/'
+  });
+}
+
+options = {
+  quotes: true
+};
+
+tree = htmlmin(tree, options);
+
+module.exports = tree
